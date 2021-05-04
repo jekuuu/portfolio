8db86163ce582528f98a425e0432ec3a20b2f713
Jekib <jekib.haque@gmail.com>
2021-05-04 22:48:13 +0530

    video responsive

--- src/Common/Videos.js
+++ src/Common/Videos.js
@@ -6,10 +6,11 @@ const Videos = ({ videoDetails }) => {
       <h5 className='dark:text-white text-xl mt-7 ml-3'>Watch Trailers</h5>
       <div className='grid grid-cols-1 md:grid-cols-3'>
         {videoDetails.slice(0, 6).map((video) => (
-          <div className='mt-3' key={video.id}>
+          <div className='mt-3 md:mr-3' key={video.id}>
             <iframe
-              width='420'
-              height='345'
+              // width='w-full md:420'
+              // height='345'
+              className='w-full h-72 md:w-96'
               src={`https://www.youtube.com/embed/${video.key}?controls=0`}
               title={video.name}
             ></iframe>
